3fb8c3533599b204cf1c684282093712065e02cd
ayanaikuta <149535647+ayanaikuta@users.noreply.github.com>
2023-11-06 18:24:48 -0300

    MySite

---
+++ src/js/links.js
@@ -0,0 +1,11 @@
+// SOCIALS
+
+const steam = () => (window.open("https://steamcommunity.com/profiles/76561199406032496/"));
+const instagram = () => (window.open("https://www.instagram.com/ayanaikkuta/"));
+const spotify = () => (window.open("https://open.spotify.com/playlist/0hQj1o7lEHoZtmirnq9D67?si=a4d1fed1df4c4fcd"));
+
+
+// OTHERS
+
+const Discord = () => (window.open("https://discord.gg/XnzwTSsS"));
+
